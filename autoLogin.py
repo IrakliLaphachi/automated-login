5ddbad8c564a10858142eae9f53ae55de0629e69
IrakliLaphachi <iraklilaphachi@gmail.com>
2025-01-26 10:48:51 +0400

    refactored autoLogin code

--- autoLogin.py
+++ autoLogin.py
@@ -4,13 +4,20 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
+
+login = driver.find_element(by=By.ID, value="login")
+password = driver.find_element(by=By.ID, value="password")
+lang = driver.find_element(by=By.ID, value="languageSelect")
+ka = driver.find_element(By.XPATH,'//*[@value="ka_GE"]')
+signIn = driver.find_element(by=By.ID, value="signInButton")
+
 driver.maximize_window()
 driver.get("http://5.152.64.228:8280/fina-app/ui/index.html#fi")
 driver.implicitly_wait(0.5)
-driver.find_element(by=By.ID, value="login").send_keys("ilapachi")
-driver.find_element(by=By.ID, value="password").send_keys("test1234")
-driver.find_element(by=By.ID, value="languageSelect").click()
-driver.find_element(By.XPATH,'//*[@value="ka_GE"]').click()
-driver.find_element(by=By.ID, value="signInButton").click()
+login.send_keys("ilapachi")
+password.send_keys("test1234")
+lang.click()
+ka.click()
+signIn.click()
 time.sleep(10)
 driver.close()
